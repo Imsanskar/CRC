eff63a51419f0f2c72ed70663d2ea46d08de2a6b
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2021-06-29 19:16:35 +0545

    command line argument for server address

--- client.cpp
+++ client.cpp
@@ -13,25 +13,46 @@
 #include <signal.h>
 
 #define PORT "6969" // port number
-#define BACKLOG 10 // total number of backlog connection
+#define BACKLOG 20 // total number of backlog connection
 
 int main(int argc, char* argv[]){
 	int socketfd, connect_fd;
 	struct addrinfo hints, *serverinfo, *itr;
 
-	memset(&hints, 0, sizeof hints);
+	// INET6_ADDRSTRLEN can store both ipv4/ipv6 addresses
+	char server_addr[INET6_ADDRSTRLEN]; 
+	bool loopback_connection = true;
 
+	// take command line argument for server address
+	// maybe need to add option for port as well, or separate this into 
+	// another function, TODO
+	switch(argc){
+		case 1:
+			strcpy(server_addr, "127.0.0.1\0"); 
+			break;
+		case 2:
+			strcpy(server_addr, argv[1]);
+			break;
+		default:
+			fprintf(stderr, 
+					"Program exited due to too many parameters. \
+					Provide only one server address to connect to.");
+			exit(1);
+			break;
+	}
+
+	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 
-	int status = getaddrinfo("127.0.0.1", "6969", &hints, &serverinfo);
+	int status = getaddrinfo(server_addr, "6969", &hints, &serverinfo);
 	if(status != 0){
 		printf("Getaddrinfo error: %s\n", gai_strerror(status));
 	}
 	
 	socketfd = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol);
 	if (socketfd == -1){
-		perror("socket error on client side")
+		perror("socket error on client side");
 	}
 
 	//connect
@@ -40,7 +61,8 @@ int main(int argc, char* argv[]){
 		perror("Connect Error");
 	}
 
-	char message[12];
-	int bytesLength = recv(socketfd, message, 12, 0);
+	char message[100];
+	int bytesLength = recv(socketfd, message, 100, 0);
+	message[bytesLength] = '\0';
 	printf("%s\n", message);
 }
